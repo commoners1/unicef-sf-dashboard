9134813b6ac88a3c3952e336d50a99486547c7b2
A Commoner <freyza.kusuma@icloud.com>
2025-11-15 08:45:18 +0700

    new-update

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -115,9 +115,6 @@ export function Sidebar({ className }: SidebarProps) {
     <div className={cn('pb-12', className)}>
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
-          {/* <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            {sidebarCollapsed ? 'SF' : 'SF Dashboard'}
-          </h2> */}
           <div className="space-y-6">
             {filteredMenuGroups.map((group, groupIndex) => (
               <div key={group.title} className="space-y-1">
